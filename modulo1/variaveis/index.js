1c59184c0118ceb958574799de63d3e9da2e89a7
Stella Neves De Castro <99159201+stella559@users.noreply.github.com>
2022-03-14 09:01:59 -0300

    Delete index.js

--- modulo1/variaveis/index.js
+++
@@ -1,5 +0,0 @@
-let SuaIdade = prompt("Qual sua idade?")
-let IdadeMelhorAmiga = prompt("Qual a idade da sua melhor amiga?")
-console.log("Qual a sua idade?",- SuaIdade)
-console.log("Qual a idade da sua melhor amiga?",- IdadeMelhorAmiga)
-
