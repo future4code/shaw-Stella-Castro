ca5e3acadf2b0a5ec21cf086ec3bb3bfeb50ea6c
unknown <stellaneves84@gmail.com>
2022-05-25 19:05:06 -0300

    intro typescript

---
+++ modulo4/intro-typescript/template-typescript/build/exercicio2.js
@@ -0,0 +1,7 @@
+function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
+    const arrayCores = [];
+    arrayCores.push(cor1, cor2, cor3);
+    return arrayCores;
+}
+console.log(imprimeTresCoresFavoritas("azul", "preto", "branco"));
+//# sourceMappingURL=exercicio2.js.map
---
+++ modulo4/intro-typescript/template-typescript/src/exercicio1.ts
@@ -0,0 +1,10 @@
+function checaTriangulo(a: Number, b: Number, c: Number): string{
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+  }
+  console.log(checaTriangulo(2,4,8));
